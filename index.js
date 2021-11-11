00b9a33b344da95bb2701b1ee591ea25a9e0a196
sanaul01 <mdsanaulislam154@gmail.com>
2021-11-11 16:31:36 +0600

    add product in database

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hcshw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,7 +16,14 @@ async function run(){
     try{
         await client.connect();
 
-        console.log('database connection successfully')
+        const database = client.db('bike_lover');
+        const productsCollection = database.collection('products');
+
+        app.post('/products', async (req, res)=>{
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.json(result)
+        })
     }
     finally {
         // await client.close();
